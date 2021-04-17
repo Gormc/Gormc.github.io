63c6f9744d7004a9cdf185279e39f5c252f32f7c
Egor Senin <81013715+Gormc@users.noreply.github.com>
2021-04-17 16:07:44 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+
+function min()
+{
+    var A = document.getElementById("a");
+    var B = document.getElementById("b");
+    
+
+    A=Number.parseInt(A);
+    B=Number.parseInt(B);
+
+    if (A>B) 
+    {
+        return("Минимальное" =B);  
+    } 
+    else 
+        return("Минимальное" =A);
+}
